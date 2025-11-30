c22179aec856f3b1ea108b2c619bf6f6e2c87382
Sumit Kumar <sumit76231@gmail.com>
2025-11-30 14:08:27 +0530

    Implemented Auth

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-import testRoute from "./routes/testRoute.js"
-dotenv.config();
-
 const app = express();
 
+import authRoute from "../src/routes/authRoute.js"
+import cookieParser from "cookie-parser"
+import testRoute from "../src/routes/testRoute.js"
+
+import dotenv from "dotenv";
+dotenv.config();
+
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/", testRoute)
+app.use("/api/auth", authRoute)
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Port started at ${port}`);
-});
+  console.log(`Server is running at ${port}`)
+})
