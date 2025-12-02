d6110028a401432dbef14f4969af65f1eccf7f58
devel-maverick <codemaverick00@gmail.com>
2025-12-02 19:22:49 +0530

    Clean up user model - remove old commented code

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,26 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     name: String, 
-//     email: String, 
-//     password: String,
-//     role: String,
-//     batch: Number,
-//     cur_role: String,
-//     company: String,
-//     location: String,
-//     pic: String,
-//     sendMessage: Array,
-//     receivedMessage: Array,
-//     lastLogin: {
-//         type: Date,
-//         default: Date.now
-//     }
-// }, { timestamps: true });
-
-// const User = mongoose.model("User", userSchema);
-
-// export default User;
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
@@ -50,7 +27,7 @@ const userSchema = new mongoose.Schema(
     },
 
     cur_role: {
-      type: String, // e.g., SDE, Intern, PM etc.
+      type: String,
     },
 
     company: {
